b291f18b17565c17d242146d2618b6c90e1a8cad
CalobTwoHearts <ctwo22342@olc.edu>
2025-07-08 01:42:19 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+// Initialize map centered over Pine Ridge Reservation
 var map = L.map('map').setView([43.3, -102.55], 9);
 
+// Base map layer (topographic)
 L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
   maxZoom: 17,
   attribution: 'Map data © OpenTopoMap & OpenStreetMap contributors'
 }).addTo(map);
 
+// River (White River)
 var riverLayer = L.geoJSON({
   "type": "FeatureCollection",
   "features": [
@@ -32,10 +35,20 @@ var riverLayer = L.geoJSON({
 
 riverLayer.bindPopup("White River");
 
-L.marker([43.3, -102.5]).addTo(map)
-  .bindPopup("Infrastructure Location")
-  .openPopup();
+// Infrastructure/Home markers (at-risk locations)
+var homesAtRisk = [
+  { name: "Oglala Town Center", coords: [43.19, -102.75] },
+  { name: "Porcupine Community", coords: [43.17, -102.58] },
+  { name: "Badlands Area Home", coords: [43.02, -102.28] }
+];
 
+homesAtRisk.forEach(function(home) {
+  L.marker(home.coords)
+    .addTo(map)
+    .bindPopup("<b>" + home.name + "</b><br>Potential flood risk area");
+});
+
+// Flood risk zones
 var floodZones = [
   {
     name: "Flood Risk Area near Oglala",
@@ -76,7 +89,7 @@ floodZones.forEach(function(zone) {
   polygon.bindPopup(zone.name);
 });
 
-// Reservation district boundary polygon example
+// Reservation district boundary polygon (example)
 var reservationDistrict = L.polygon([
   [43.35, -102.85],
   [43.35, -102.45],
@@ -90,3 +103,19 @@ var reservationDistrict = L.polygon([
 }).addTo(map);
 
 reservationDistrict.bindPopup("Reservation District Boundary");
+
+// Legend
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+  var div = L.DomUtil.create('div', 'info legend');
+  div.innerHTML += "<h4>Legend</h4>";
+  div.innerHTML += '<i style="background: red"></i> Flood Risk Zone<br>';
+  div.innerHTML += '<i style="background: #0077cc"></i> River<br>';
+  div.innerHTML += '<i style="background: green"></i> Reservation Boundary<br>';
+  div.innerHTML += '<svg width="18" height="18"><circle cx="9" cy="9" r="7" fill="black"/></svg> Infrastructure/Home<br>';
+  return div;
+};
+
+legend.addTo(map);
+
