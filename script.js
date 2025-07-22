fdd506e6a83c36170a5f1e0de7c30944f3d5e097
CalobTwoHearts <ctwo22342@olc.edu>
2025-07-22 07:43:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -27,46 +27,6 @@ L.tileLayer.wms('https://basemap.nationalmap.gov/arcgis/services/Contours/MapSer
   zIndex: 110
 }).addTo(map);
 
-// --- Streamflow Data (USGS WaterWatch WMS) ---
-L.tileLayer.wms('https://waterwatch.usgs.gov/arcgis/services/Streamflow/MapServer/WMSServer', {
-  layers: '1',
-  format: 'image/png',
-  transparent: true,
-  attribution: 'USGS Streamflow Data',
-  opacity: 0.6,
-  zIndex: 120
-}).addTo(map);
-
-// --- Historical Flood Events (USGS Flood Event Viewer GeoJSON) ---
-// Using a bounding box roughly covering Oglala, Manderson, Wounded Knee area
-fetch('https://stn.wim.usgs.gov/FEV/api/Events/GeoJson?bbox=-102.85,43.10,-102.35,43.30')
-  .then(response => response.json())
-  .then(data => {
-    L.geoJSON(data, {
-      onEachFeature: function(feature, layer) {
-        var props = feature.properties;
-        var popupContent = `<b>Flood Event</b><br>
-          Name: ${props.eventName || "N/A"}<br>
-          Start Date: ${props.startDate || "N/A"}<br>
-          Description: ${props.description || "No description available."}`;
-        layer.bindPopup(popupContent);
-      },
-      pointToLayer: function(feature, latlng) {
-        return L.circleMarker(latlng, {
-          radius: 6,
-          fillColor: "#ff7800",
-          color: "#000",
-          weight: 1,
-          opacity: 1,
-          fillOpacity: 0.8
-        });
-      }
-    }).addTo(map);
-  })
-  .catch(error => {
-    console.error("Failed to load flood events GeoJSON:", error);
-  });
-
 // --- Town Markers ---
 var towns = [
   { name: "Oglala", coords: [43.19, -102.75] },
@@ -89,23 +49,6 @@ towns.forEach(function(town) {
       className: 'town-label'
     });
 });
-
-// --- Reservation Boundary ---
-fetch("https://sdgis.sd.gov/host/rest/services/Hosted/Boundary_ReservationBoundariesAndTribalLands/FeatureServer/0/query?where=NAME%3D'Pine+Ridge+Reservation'&f=geojson")
-  .then(response => response.json())
-  .then(data => {
-    L.geoJSON(data, {
-      style: {
-        color: '#8800cc',
-        fillColor: '#cc99ff',
-        fillOpacity: 0.3,
-        weight: 2
-      }
-    }).addTo(map).bindPopup("Pine Ridge Reservation Boundary");
-  })
-  .catch(error => {
-    console.error("Failed to load reservation boundary:", error);
-  });
 
 // --- White River Flood Buffer Zone (embedded GeoJSON) ---
 var riverBuffer = {
@@ -170,12 +113,10 @@ legend.onAdd = function(map) {
   var div = L.DomUtil.create('div', 'info legend');
   div.innerHTML += "<h4>Legend</h4>";
   div.innerHTML += '<i style="background: #4477ff"></i> Elevation Contours<br>';
-  div.innerHTML += '<i style="background: #ff7800"></i> Historical Flood Events<br>';
-  div.innerHTML += '<i style="background: #0080ff"></i> Streamflow Data<br>';
-  div.innerHTML += '<i style="background: #cc99ff"></i> Reservation Boundary<br>';
   div.innerHTML += '<i style="background: #aaddff"></i> White River Flood Buffer<br>';
   div.innerHTML += '<svg width="18" height="18"><circle cx="9" cy="9" r="7" fill="black"/></svg> Town Marker<br>';
   return div;
 };
 legend.addTo(map);
+
 
